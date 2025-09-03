fb8bf5ef5af16b19c1f39c0e469872feefc83f6f
Kiim <kms545487@gmail.com>
2025-09-03 21:58:15 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const exportFormat = document.getElementById('export-format');
+const fileName = document.getElementById('file-name');
+const exportButton = document.getElementById('export-button');
+
+exportButton.addEventListener('click', () => {
+    if (exportFormat.value === '') {
+        alert('선택하세요');
+        return;
+    }
+    if (fileName.value === '' || fileName.value.includes(' ')) {
+        alert('파일 이름을 입력하세요(공백 없이)');
+        return;
+    }
+    const file_type = exportFormat.value;
+    const filename = fileName.value;
+
+    exportRequest(file_type, filename);
+    alert('완료');
+});
+
+exportRequest = async (file_type, filename) => {
+    const response = await fetch('https://tickercrawler-production.up.railway.app/export', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ file_type, filename }),
+    });
+    if (response.ok) {
+        alert('완료');
+    } else {
+        alert('실패');
+    }
+};
